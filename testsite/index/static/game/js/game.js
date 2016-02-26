be148cef19789bdc0e1a4924fdce8988ecdf47cc
evan <mistix@comcast.net>
2016-02-25 17:10:13 -0700

    Added better comments on game.js

--- testsite/index/static/game/js/game.js
+++ testsite/index/static/game/js/game.js
@@ -1,16 +1,21 @@
 
 
-
+//This is what starts the game by making a new Phaser game object
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'TEST', { preload: preload, create: create, update: update });
 
+//This functino loads all of the images used in the game ahead of time. It assigns each of them a tag which you can use to reference it.
 function preload() {
     game.load.image('e',STATIC_URL+'images/ship.png');
     game.load.image('b',STATIC_URL+'images/bullet.png');
     game.load.image('a',STATIC_URL+'images/asteroid.png');
 	game.load.spritesheet('pause_button', STATIC_URL+'images/pause.png', 30, 30);
     
+
+//Keep the spacebar from being picked up by the website so it doesn't scroll down.
 	game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
 }
+
+//Declare variables
 var eSprite;
 var asteroid;
 var asteroids;
@@ -24,66 +29,63 @@ var i;
 var rand;
 var p_button; //pause button
 
+//This function is where we create all of our sprites, start up processes, etc.
 function create() {
+
+	//This sets how the game handles collisions, etc.
 	game.physics.startSystem(Phaser.Physics.ARCADE);
+     game.input.enabled = true;
+
 
     s = game.add.sprite(game.world.randomX, game.world.randomY, 'e');
     game.physics.arcade.enable(s);
     s.body.velocity.y = 10;
-     game.input.enabled = true;
+
+	//Group of bullets
     bullets = game.add.group();
     bullets.enableBody = true;
 	bullets.physicsBodyType = Phaser.Physics.ARCADE;
 	
+	//Group of asteroids
 	asteroids = game.add.group();
 	asteroids.enableBody = true;
 	asteroids.physicsBodyType = Phaser.Physics.ARCADE;
 	
-	
-	
-    eSprite = game.add.sprite(100,100,'e');
-    
-    //This is the minumum you have to do to add a sprite to the game.
-
-	   game.physics.arcade.enable(eSprite);
-//    game.physics.arcade.enable(asteroid);
-	
+	//Add asteroids to the asteroid group and then set their physics properties
 	for(var k  = 0; k < 5; k++)
 	{
     	var ast = asteroids.create(game.world.randomX, game.world.randomY, 'a');
 		 
 	}
     
+	//The anchor of a sprite is where its center is relative to the image. x=0,y=0 is the top left corner. x=1,y=1 is the bottom right corner
 	asteroids.setAll('anchor.x',.5);
 	asteroids.setAll('anchor.y',.5);
 	asteroids.setAll('body.collideWorldBounds', true);
-	
-    //Enable physics on these two sprites
- 
-    
-    cursors = game.input.keyboard.createCursorKeys();
+		
 
-
-	
-//    asteroid.body.gravity.y = 0;
-//    asteroid.body.gravity.x = 0;
-//    asteroid.body.collideWorldBounds = true;
-//    asteroid.anchor.setTo(0.5,0);
-   
-
+	//Player
+    eSprite = game.add.sprite(100,100,'e');
+    game.physics.arcade.enable(eSprite);
     eSprite.body.gravity.y = 0;
     eSprite.body.gravity.x = 0;
     eSprite.body.collideWorldBounds = true;
     eSprite.anchor.setTo(0.5, 0.5);
-    
-	 game.time.events.loop(Phaser.Timer.SECOND*10, moveAsteroid, this);
+
 	
+	//This allows for arrow key input    
+    cursors = game.input.keyboard.createCursorKeys();
+
+	//Make the asteroids move by continually callng moveAsteroid()
+	 game.time.events.loop(Phaser.Timer.SECOND*10, 
+moveAsteroid, this);
 	
+	//fire button
     key2 = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     key2.onDown.add(fire, this);
     
     
-    //Here
+	//this button creates a non moving bullet
     key1 = game.input.keyboard.addKey(Phaser.Keyboard.ONE);
     key1.onDown.add(addPhaserDude,this);   
 
@@ -106,6 +108,7 @@ function actionOnClick() {
 }
 /* end pause button */
 
+
 var xGlidePos = 0;
 var xGlideNeg = 0;
 var yGlidePos = 0;
@@ -113,8 +116,11 @@ var yGlideNeg = 0;
 var asteroidTime = 0;
 
 function update() {
+
+	//Handle overlaps between members of the asteroids and bullets groups. It calls the collisionHandler method.
 	game.physics.arcade.overlap(bullets, asteroids, collisionHandler, null, this);
-	
+
+/*~~~~~~~~~~~~~~~~~Arrow keys~~~~~~~~~~~~~~~~~~~*/	
     if (cursors.left.isDown)
     {
          if(!cursors.up.isDown&&!cursors.down.isDown)
@@ -200,53 +206,58 @@ function update() {
             {
                 eSprite.angle = -45;
             } 
+/*~~~~~~~~~~~~~~~~~Arrow keys~~~~~~~~~~~~~~~~~~~*/
   
 	
 }
 
+
+//this is the function that is called to make a non moving bullet. Doesn't really do anything.
 function addPhaserDude () {
-//    
     game.add.sprite(eSprite.x, eSprite.y, 'b');
 }
 
+//Create and fire a bullet
     function fire()
     {
-//          var panda = pandas.create(game.world.randomX, game.world.randomY, 'panda');
-//      var newBullet = game.add.sprite(eSprite.x, eSprite.y, 'b');
+		//create a new bullet in the bullets group and place it at the ships position
 		var newBullet = bullets.create(eSprite.x, eSprite.y, 'b');
-//        game.physics.arcade.enable(newBullet);
 		newBullet.anchor.setTo(0.5,1);
 		newBullet.events.onOutOfBounds.add(resetBullet, this);
 
         newBullet.angle = eSprite.angle;
-//        newBullet.body.velocity.y = -50;
-        newBullet.angle = eSprite.angle;
+	
+	//Make the bullet move in the direction the ship is facing with a speed of 1000
         game.physics.arcade.velocityFromAngle(eSprite.angle-90, 1000, newBullet.body.velocity);
         
     }
 
+//This function moves all of the members of the asteroids group
 function moveAsteroid()
 {
+	
 	for(var i = 0; i < asteroids.children.length; i ++)
 		{
 			if(asteroids.children[i].alive)
 				{ 
+					//Point in random angle
 					asteroids.children[i].angle += game.rnd.integerInRange(-180, 180);
-					game.physics.arcade.velocityFromRotation(asteroids.children[i].rotation, 50, asteroids.children[i].body.velocity);
-//					console.log(rand.angle());
-//					asteroids.children[i].angle = 
-//							game.physics.arcade.moveToXY(asteroids.childen[i], game.world.randomX, game.world.randomY,300,5000);
-////game.time.events.loop(Phaser.Timer.SECOND * 5, moveAsteroid(asteroid), this);
-				}
+
+	//Go forwards				
+	game.physics.arcade.velocityFromRotation(asteroids.children[i].rotation, 50, asteroids.children[i].body.velocity);
+	game.physics.arcade.moveToXY(asteroids.childen[i], game.world.randomX, game.world.randomY,300,5000);
+		}
 		}
 
 }
 
+//This is called when bullets go out of the screen. It removes any bullets not on the screen.
 function resetBullet (bul)
 {
 	bul.kill();
 }
 
+//This removes a bullet and an asteroid that collide.
 function collisionHandler(bul, ast)
 {
 	bul.kill();
