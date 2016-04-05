0b50a151100ac06fa9d770385451821fb94e9708
JerryZhangJingyi <jerryzhangjingyi@gmail.com>
2016-04-05 16:22:40 -0600

    game with the music

--- mysite/index/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from models import UserProfile
-
-class UserProfileForm(forms.ModelForm):
-
-    class Meta:
-        model = UserProfile
-        #fields = ('if_student', 'score')
-        fields = ('score',)
