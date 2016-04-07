aafdaa33878690fa1ecb731176ec1d9085387be3
JerryZhangJingyi <jerryzhangjingyi@gmail.com>
2016-04-07 15:38:17 -0600

    test

--- project/SpaceSpeller/game1/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class Game1Config(AppConfig):
-    name = 'game1'
--- project/SpaceSpeller/mainpage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.loadMain, name='main'),
-    url(r'^slogin/$', views.studentLogin),
-    url(r'^tlogin/$', views.teacherLogin),
-]
