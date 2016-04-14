2ca212c08265b7055efaeb6a96eb4994c3c8dfc9
Xuan Mai <xtm1109@westminstercollege.edu>
2016-04-14 16:54:38 -0600

    fixed student and teacher account ability of loading certain pages

--- site/SpaceSpeller/student/views.py
+++ site/SpaceSpeller/student/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+
+"""
+Check if logged-in account is a student account
+"""
+def isStudent(user):
+    return user.groups.filter(name='Student').exists()
+
 
 @login_required
+@user_passes_test(isStudent)
 def studentDashboard(request):
     return render(request, 'student/dashboard.html', { })
--- site/SpaceSpeller/teacher/views.py
+++ site/SpaceSpeller/teacher/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from datetime import datetime
 from django.core import serializers
 from django.contrib.auth.models import User, Group
@@ -8,29 +8,6 @@ from django.http import HttpResponse
 import game1.models
 import json
 
-@login_required
-def teacherPage(request):
-    std = game1.models.StudentInfo.objects.all()
-    score = game1.models.ScoreInfo.objects.filter(student_id=std[0]).values_list("recordtime", "score")
-    data = []
-    json_date = []
-    test = {}
-
-    for i in range(0, std.count()):
-        score = game1.models.ScoreInfo.objects.filter(student_id=std[i]).values_list("recordtime", "score")
-        
-        for j in range(0, len(score)):
-            dt = json_serial(score[j][0])
-            json_date.append((dt, score[j][1]))
-
-        test[std[i].username] = json_date
-        json_date = []
-
-    data = serializers.serialize("json", std)
-    score_list = json.dumps(test)
-
-    return render(request, 'teacher/teacher.html', {'ex': data, 'ex2': score_list, 'tfn': json.dumps(request.user.username)})
-
 
 """JSON serializer for objects not serializable by default json code"""
 def json_serial(obj):
@@ -38,6 +15,7 @@ def json_serial(obj):
         serial = obj.isoformat()
         return serial
     raise TypeError ("Type not serializable")
+
 
 """
 Create a student login
@@ -67,6 +45,10 @@ def addStudent(request):
         
     return HttpResponse('')
 
+
+"""
+Delete a chosen student
+"""
 def removeStudent(request):
     if request.method == "POST":
         studentid = request.POST['sid']
@@ -80,3 +62,35 @@ def removeStudent(request):
 
 
     return HttpResponse('')
+
+
+"""
+Check if logged-in account is a teacher account
+"""
+def isTeacher(user):
+    return user.groups.filter(name='Teacher').exists()
+
+
+@login_required
+@user_passes_test(isTeacher)
+def teacherPage(request):
+    std = game1.models.StudentInfo.objects.all()
+    score = game1.models.ScoreInfo.objects.filter(student_id=std[0]).values_list("recordtime", "score")
+    data = []
+    json_date = []
+    test = {}
+
+    for i in range(0, std.count()):
+        score = game1.models.ScoreInfo.objects.filter(student_id=std[i]).values_list("recordtime", "score")
+        
+        for j in range(0, len(score)):
+            dt = json_serial(score[j][0])
+            json_date.append((dt, score[j][1]))
+
+        test[std[i].username] = json_date
+        json_date = []
+
+    data = serializers.serialize("json", std)
+    score_list = json.dumps(test)
+
+    return render(request, 'teacher/teacher.html', {'ex': data, 'ex2': score_list, 'tfn': json.dumps(request.user.username)})
